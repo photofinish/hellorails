5baf65a831653e3e20c7582e69070e62c0d06e05
photofinish <dangs456789@sina.com>
2016-10-04 21:49:53 +0800

    i

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -30,7 +30,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
