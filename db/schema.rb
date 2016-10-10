df4e4f4f0897a2eb69d26a02b908c9a704ffde05
Photofinish <dangs456789@sina.com>
2016-10-10 13:52:39 +0000

    i

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20161010100531) do
     t.boolean  "admin",             default: false
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
-    t.time     "activated_at"
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
