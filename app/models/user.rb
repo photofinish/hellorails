e0e9af365950be72fd8b5ca22595bd0930c68da6
Photofinish <dangs456789@sina.com>
2016-10-17 04:26:52 +0000

    i

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_create :create_activation_digest
   before_save { :downcase_email }
@@ -9,7 +10,10 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
