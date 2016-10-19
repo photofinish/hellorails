2a9690d7211dc5a1c2c5c22d75ac5ace4d2e5dab
Photofinish <dangs456789@sina.com>
2016-10-19 03:48:09 +0000

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
            foreign_key: "follower_id",
            dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+           foreign_key: "followed_id",
+           dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
   attr_accessor :remember_token, :activation_token, :reset_token
   before_create :create_activation_digest
   before_save { :downcase_email }
@@ -13,8 +18,21 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "select followed_id from relationships where follower_id = :user_id"
+    Micropost.where("user_id in (#{following_ids}) or user_id = :user_id",user_id: id)
   end
   
   def remember
